0a5e644d398d6b5c9b17bec385348ee6db9822ed
SongsShao <1175715363@qq.com>
2020-08-21 17:17:47 +0800

    初始化项目

---
+++ precache-manifest.f788d2019ab6b05703e0a2eeb4cd005b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737f7abefd198dba3d15ed2ccb93be4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6839a37b25b2320083",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "f610c0c267f01993cf60",
+    "url": "/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "2e6839a37b25b2320083",
+    "url": "/static/js/2.13f592f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.13f592f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f610c0c267f01993cf60",
+    "url": "/static/js/main.b27abf13.chunk.js"
+  },
+  {
+    "revision": "85f41a3108a9b6db6832",
+    "url": "/static/js/runtime-main.d82219d9.js"
+  }
+]);
